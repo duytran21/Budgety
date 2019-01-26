060fda473975dd783e5fc673f534a46ae81b099f
duytran21 <alanduy21@yahoo.com>
2019-01-26 01:24:24 -0800

    Adding controller delete item function

--- app.js
+++ app.js
@@ -89,6 +89,25 @@ var budgetController = (function (){
 			}
 		},
 
+		deleteBudget: function(type, id){
+			var ids, index;
+			// id = 6
+			// data.allItems[type][id];
+			// [1 2 4 6 8]
+			// index = 3
+
+			ids = data.allItems[type].map(function(current){ //map will return brand new array
+				return current.id; // will return [1 2 4 6 8] this is an example for tutor to show us how to use map. It's not necessary here
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1){
+				data.allItems[type].splice(index, 1); // [1 2 4 - 8];
+			}
+
+		},
+
 
 		testing: function(){
 			console.log(data);
@@ -109,7 +128,12 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage',
+		container: '.container'
 
 	};
 	return {
@@ -126,10 +150,10 @@ var UIController = (function() {
 			// Create HTML string with placeholder text
 			if (type === 'inc'){
 				element = DOMstrings.incomeContainer;
-				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+				html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 			} else if (type === 'exp'){
 				element = DOMstrings.expensesContainer;
-				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+				html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 			}
 
 			// Replace the placeholder text with some actual data
@@ -157,6 +181,21 @@ var UIController = (function() {
 
 		},
 
+		displayBudget: function(obj){
+			if(obj.budget > 0){
+				document.querySelector(DOMstrings.budgetLabel).textContent = '+ ' + obj.budget;
+			} else {
+				document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			}
+			document.querySelector(DOMstrings.incomeLabel).textContent = '+ ' + obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = '- ' + obj.totalExp;
+			if(obj.percentage > 0){
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else{
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
+		},
+
 		getDOMstrings: function(){
 			return DOMstrings;
 		}
@@ -181,6 +220,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 				ctrlAddItem();
 			}
 		});
+
+		// Using Event Delegation for this case
+		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
 	};
 
 	var updateBudget = function(){
@@ -192,7 +235,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-		console.log(budget);
+		//console.log(budget);
+		UICtrl.displayBudget(budget);
+
 	}
 	
 	//Controler Add Item funciton
@@ -216,9 +261,36 @@ var controller = (function(budgetCtrl, UICtrl) {
 		
 	};
 
+	var ctrlDeleteItem = function(event){
+		//console.log(event.target);
+		var itemID, splitID, type, ID;
+
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+		if (itemID) {
+			splitID = itemID.split('-');
+			type = splitID[0];
+			ID = parseInt(splitID[1]);
+
+			// 1. Delete the item from datastruture.
+			budgetCtrl.deleteBudget(type, ID);
+
+			// 2. Delete the item from the UI.
+
+
+			// 3. Update and show the new budget.
+		}
+	}
+
 	return {
 		init: function(){
 			console.log("Application has started!");
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListener();
 		}
 	}
